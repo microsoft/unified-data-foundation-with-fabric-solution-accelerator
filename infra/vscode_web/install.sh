56d998eb03d7b8370ed99856faf5375ce13b375a
Ragini-Microsoft <v-raginich@microsoft.com>
2025-11-26 15:37:07 +0530

    Revert "added feat branch for testing"
    This reverts commit a18e95afac077d017369df70c474d25e7e09bf91.

--- infra/vscode_web/install.sh
+++ infra/vscode_web/install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt --user -q
 
-azd init -t microsoft/unified-data-foundation-with-fabric-solution-accelerator -b maag-vscodeweb
+azd init -t microsoft/unified-data-foundation-with-fabric-solution-accelerator
