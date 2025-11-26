a18e95afac077d017369df70c474d25e7e09bf91
Ragini-Microsoft <v-raginich@microsoft.com>
2025-11-26 14:44:00 +0530

    added feat branch for testing

--- infra/vscode_web/install.sh
+++ infra/vscode_web/install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt --user -q
 
-azd init -t microsoft/unified-data-foundation-with-fabric-solution-accelerator
+azd init -t microsoft/unified-data-foundation-with-fabric-solution-accelerator -b maag-vscodeweb
